38302719b229f59a2385f75750328a1600bb461d
Manoj-Dwivedi <manojdwivedi360@gmail.com>
2025-07-29 11:44:38 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          flowbite: ['flowbite-react'],
-        }
-      }
-    }
-  }
+  plugins: [react(), tailwindcss()],
 });
