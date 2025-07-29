dac5bba497b4438b72e58194098a78d0df2e6939
Manoj-Dwivedi <manojdwivedi360@gmail.com>
2025-07-29 11:54:10 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Eccommerce-ssunique/',
   plugins: [react(), tailwindcss()],
+   build: {
+    chunkSizeWarningLimit: 1000, // optional
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        }
+      }
+    }
+  }
 });
